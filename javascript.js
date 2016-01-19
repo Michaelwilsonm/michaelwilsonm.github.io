fdcd46602a0109654a72c2032edf21bead8e144d
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-01-20 11:24:08 +1300

    Add functional menu bar and nav with responsivness

--- javascript.js
+++ javascript.js
@@ -1,17 +1,33 @@
 $(document).ready(function(){
 
   $(".dropbutton").on("click", function(){
-    $(".dropdown-content").toggle(700)
+    $(".dropdown-content").toggle(500)
   })
-  // if (matchMedia) {
-  //   var mediaQuery = window.matchMedia( "(max-width: 750px)" );
-  //   mediaQuery.addListener(widthChange)
-  //   widthChange.mediaQuery
-  // }
 
-  // function widthChange(mediaQuery) {
-  //   if (mediaQuery.matches) {
-  //     $(".top-nav").addClass("active")
-  //   }
+  if (matchMedia) {
+    var mediaQuery = window.matchMedia( "(max-width: 750px)" );
+    mediaQuery.addListener(widthChange)
+    widthChange.mediaQuery
+  }
+  if (mediaQuery.matches == true) {
+    $(".dropbutton").show()
+    $(".nav-list li a").css("display", "none")
+  }
+  else{
+    $(".dropbutton").hide()
+    $(".nav-list li a").show()
+  }
+
+  function widthChange(mediaQuery) {
+    if (mediaQuery.matches) {
+      $(".dropbutton").show()
+      $(".nav-list li a").css("display", "none")
+    }
+    else {
+      $(".dropdown-content").hide()
+      $(".dropbutton").hide()
+      $(".nav-list li a").show()
+    }
+  }
 
 })//end of ready
