86710e6109c1c611cb0053a1dc59ea7f63383ce6
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-01-19 22:16:15 +1300

    add menu toggle

--- javascript.js
+++ javascript.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
 
+  $(".dropbutton").on("click", function(){
+    $(".dropdown-content").toggle(700)
+  })
+  // if (matchMedia) {
+  //   var mediaQuery = window.matchMedia( "(max-width: 750px)" );
+  //   mediaQuery.addListener(widthChange)
+  //   widthChange.mediaQuery
+  // }
 
-
-
-
+  // function widthChange(mediaQuery) {
+  //   if (mediaQuery.matches) {
+  //     $(".top-nav").addClass("active")
+  //   }
 
 })//end of ready
