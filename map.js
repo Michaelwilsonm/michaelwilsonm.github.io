c18854cd8e105538a049266c687f7b12128624d2
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-01-22 20:56:05 +1300

    add markers

--- map.js
+++ map.js
@@ -1,21 +1,25 @@
 var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 0, lng: 0},
-    zoom: 1,
-    mapTypeId: google.maps.MapTypeId.SATELLITE
-  });
-}
-
-var map;
-function initMapDropPin(latitude, longitude) {
+function initMap(latitude, longitude) {
   console.log(latitude, longitude)
+  if (latitude || longitude === undefined) {
+    latitude = 0
+    longitude = 0
+  }
+  else {
+    var myLatLng = {lat: latitude, lng: longitude};
+  }
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: latitude, lng: longitude},
     zoom: 1,
     mapTypeId: google.maps.MapTypeId.SATELLITE
   });
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+  });
 }
+
+// setInterval(issData, 6000)
 
 function issData(){
   $.ajax({
@@ -23,18 +27,19 @@ function issData(){
     url: "http://api.open-notify.org/iss-now.json",
     dataType: 'jsonp',
     success: function(data) {
+      console.log(data)
       var lat = data.iss_position.latitude
       var lng = data.iss_position.longitude
-      initMapDropPin(lat, lng)
+      initMap(lat, lng)
     }
   })
 }
+issData()
 
 // function googleMaps(data){
 //   console.log(data)
 // }
 
-issData()
 //       console.log(lat, lng)
 //       var key = "&key=" + "AIzaSyDO-dchbaQweyou9F2gjmHF3bvH3YQ8VaE"
 //       var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + key
