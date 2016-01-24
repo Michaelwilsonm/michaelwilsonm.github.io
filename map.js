184691a8765568987ff7a012980b389c7e4c2331
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-01-24 20:32:28 +1300

    plots where space station is moving over world map

--- map.js
+++ map.js
@@ -1,45 +1,40 @@
-var map;
-function initMap(latitude, longitude) {
-  console.log(latitude, longitude)
-  if (latitude || longitude === undefined) {
-    latitude = 0
-    longitude = 0
+var map
+
+setInterval(issData, 7000)
+function issData(){
+  $.ajax({
+    method: "GET",
+    url: "http://api.open-notify.org/iss-now.json",
+    dataType: 'jsonp',
+    success: latLong
+  }).fail(function(err){
+    console.log(err.statusText)
+  })
+}
+
+function latLong(data){
+  var iss_pos = {
+    lat: data.iss_position.latitude,
+    lng: data.iss_position.longitude
   }
-  else {
-    var myLatLng = {lat: latitude, lng: longitude};
-  }
+  dropPin(iss_pos)
+}
+
+function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: latitude, lng: longitude},
+    center: {lat: 17.0000, lng: 17.00000},
     zoom: 1,
-    mapTypeId: google.maps.MapTypeId.SATELLITE
   });
+}
+
+function dropPin(coords) {
+  var myLatLng = coords
+  console.log(coords)
   var marker = new google.maps.Marker({
     position: myLatLng,
     map: map,
   });
 }
 
-// setInterval(issData, 6000)
 
-function issData(){
-  $.ajax({
-    method: "GET",
-    url: "http://api.open-notify.org/iss-now.json",
-    dataType: 'jsonp',
-    success: function(data) {
-      console.log(data)
-      var lat = data.iss_position.latitude
-      var lng = data.iss_position.longitude
-      initMap(lat, lng)
-    }
-  })
-}
 issData()
-
-// function googleMaps(data){
-//   console.log(data)
-// }
-
-//       console.log(lat, lng)
-//       var key = "&key=" + "AIzaSyDO-dchbaQweyou9F2gjmHF3bvH3YQ8VaE"
-//       var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + key
