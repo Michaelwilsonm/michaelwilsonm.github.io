56d85932c02d1ab3d842e0e42eaaf499ac94f00d
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-02-23 17:55:18 +1300

    add text remove lorem

--- scrolling-mediaquery.js
+++ scrolling-mediaquery.js
@@ -82,4 +82,9 @@ $(document).ready(function(){
   event.preventDefault();
   $('html,body').animate({ scrollTop:$(this.hash).offset().top }, 1000);
   })
+
+  $(".scroll").click(function(event) {
+  event.preventDefault();
+  $('html,body').animate({ scrollTop:$(this.hash).offset().top }, 1000);
+  })
 })
