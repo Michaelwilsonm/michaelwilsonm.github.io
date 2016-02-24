9eae5270951cba1782883acc16f01e1042452112
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-02-24 18:08:26 +1300

    Add some logic for cards - halfway through

--- card-styling.js
+++ card-styling.js
@@ -1,6 +1,28 @@
 $(document).ready(function(){
 
-  console.log('hello')
+  $(".thumbnail").hover(function(){
+    $img = $(this).find('img')
+    if ($(this).hasClass("one")) {
+      $img.animate({
+        width: "250px",
+        height: "150px"
+      }, 1000)
+    }else if ($(this).hasClass("two")) {
+      $img.animate({
+        width: "265px",
+        height: "160px"
+      }, 1000)
+    }else{ $(this).hasClass("three")
+      $img.animate({
+        width: "250px",
+        height: "160px"
+      }, 1000)
+    }
+  },function(){
+    console.log($(this))
+  });
+
+
 
 
 
