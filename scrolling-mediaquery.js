5f68551832b5b6a44f87d955268ea54f6de7d909
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-02-25 21:01:50 +1300

    add mobile compatibility

--- scrolling-mediaquery.js
+++ scrolling-mediaquery.js
@@ -3,13 +3,14 @@ $(document).ready(function(){
   //media querys
   if (matchMedia) {
     var mobileQuery = window.matchMedia( "(max-device-width: 480px)" )
-    console.log(mobileQuery)
     var mediaQuery = window.matchMedia( "(max-width: 768px)" );
-    console.log(mediaQuery)
     mediaQuery.addListener(widthChange)
+    mediaQuery.addListener(mobileChange)
     widthChange.mediaQuery
     widthChange.mobileQuery
+    mobileChange.mobileQuery
   }
+
 
   if (mediaQuery.matches == true || mobileQuery.matches == true) {
     $("#nav-icon1").show()
@@ -21,8 +22,24 @@ $(document).ready(function(){
     $(".nav-list li a").show()
   }
 
+  if (mobileQuery.matches == true) {
+    $(".profile").removeClass("col-sm-6")
+    $(".profile").addClass("col-xs-12")
+    $(".skills").removeClass("col-sm-6")
+    $(".skills").addClass("col-xs-12")
+    $(".col-md-4").addClass("col-xs-12")
+    $(".col-md-4").removeClass("col-sm-6")
+    $(".col-md-4").addClass("col-xs-12")
+    $(".col-md-4").removeClass("col-sm-6")
+    $(".non-scrolling-background").removeClass()
+    $(".bottom-breakpoints_1").removeClass()
+
+  }
+
+  function mobileChange(query) {
+  }
+
   function widthChange(query) {
-    console.log(query)
     if (query.matches) {
       $("#nav-icon1").show()
       $(".nav-list li a").css("display", "none")
