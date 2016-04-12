344c52d86be10e218743c74d90a6aa0a625a2c9d
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-04-13 11:24:17 +1200

    add quote fade in

--- card-styling.js
+++ card-styling.js
@@ -1,48 +1,54 @@
 
 function fullWidthCards() {
   $(window).scroll(function() {
-    var pos = ($("#projects")[0].offsetTop - 450)
+    var pos = ($("#projects")[0].offsetTop - 500)
+    var quotePos = ($("#quote-hide")[0].offsetTop - 400)
     var windowPos = $(window).scrollTop();
 
-    if (windowPos > pos ){
-      $(".two>img").animate({
-        "display": "block",
-        "left": "0",
-        "opacity": "1"
-      }, 800)
-      $(".one>img").animate({
-        "left": "0",
-        "opacity": "1"
-      }, 800)
-      $(".code").fadeIn(1500)
+    if (windowPos > pos){
+      $(".thumbnail").animate({
+        "opacity": "1",
+        "left": "0"
+      }, 700)
+      $(".code").fadeIn(1400)
+    }
+    if (windowPos > quotePos){
+      $(".quote").animate({
+        "opacity": "1",
+      }, 700)
     }
   })
 }
 
 function halfWidthCards(){
   $(window).scroll(function() {
-    var pos = ($("#projects-0")[0].offsetTop - 300)
-    var pos1 = ($("#projects-1")[0].offsetTop - 300)
-    var pos2 = ($("#projects-2")[0].offsetTop - 300)
+    var pos = ($("#projects-0")[0].offsetTop - 400)
+    var pos1 = ($("#projects-1")[0].offsetTop - 400)
+    var pos2 = ($("#projects-2")[0].offsetTop - 400)
+    var pos3 = ($("#quote-hide")[0].offsetTop - 400)
     var windowPos = $(window).scrollTop();
-    console.log(pos2)
-    console.log(windowPos)
 
     if (windowPos > pos) {
-      $(".one>img").animate({
-        "left": "0",
-        "opacity": "1"
-      }, 800)
+      $(".one").animate({
+        "opacity": "1",
+        "left": "0"
+      }, 500)
     }
     if (windowPos > pos1) {
-      $(".code").fadeIn(1500)
-    }
-    if (windowPos > pos2) {
-      $(".two>img").animate({
-        "left": "0",
+      $(".three").animate({
         "opacity": "1"
       }, 800)
+    }
+    if (windowPos > pos2) {
+      $(".two").animate({
+        "opacity": "1",
+        "left": "0"
+      }, 500)
+    }
+    if (windowPos > pos3) {
+      $(".quote").animate({
+        "opacity": "1",
+      }, 700)
     }
   })
 }
-
