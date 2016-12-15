f51eddd3ce95d8db4043f3596d380d4b2850cac2
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-12-15 22:57:21 +1300

    add clock

--- main.js
+++ main.js
@@ -7,5 +7,25 @@ window.addEventListener('keydown', function(e){
 
 function introSong(){
   const intro = document.querySelector("audio[data-key=intro]");
-  intro.play()
+  // intro.play()
 }
+
+
+const secondsDiv = document.querySelector('.seconds')
+const hoursDiv = document.querySelector('.hours')
+const minutesDiv = document.querySelector('.minutes')
+
+function getDate(){
+  const date = new Date()
+  const seconds = date.getSeconds()
+  const minutes = date.getMinutes()
+  const hours = date.getHours()
+  const minutesDegrees = ((minutes / 60) * 360) + 90;
+  const hoursDegrees = ((hours / 24) * 360) + 90;
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondsDiv.style.transform = "rotate(" + secondsDegrees + "deg)"
+  minutesDiv.style.transform = "rotate(" + minutesDegrees + "deg)"
+  hoursDiv.style.transform = "rotate(" + hoursDegrees + "deg)"
+}
+getDate()
+setInterval(getDate, 1000);
